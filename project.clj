e4cbb51b779c298a983cce977bf640da5d9fd15b
root <root@raspberrypi.(none)>
2013-02-17 16:14:27 +0100

    repl options for remote nrepl

--- project.clj
+++ project.clj
@@ -3,5 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repl-options {:port 9999
+		 :host "0.0.0.0"
+		 :timeout 240000}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [framboos/framboos "0.0.1-SNAPSHOT"]])
